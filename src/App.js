a99e1bf7caefad4d8221d50465c1abb888e90b22
Raouf Satto <64332249+RaoufSEZAR@users.noreply.github.com>
2021-01-09 12:40:38 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-// import { AppearanceProvider, useColorScheme } from 'react-native-appearance';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {CategoriContent, Categories ,MeelDetail} from './pages';
@@ -9,20 +8,16 @@ import { Header } from 'react-native/Libraries/NewAppScreen';
 const Stack = createStackNavigator();
 // export default () => {
 function App() {
-  // const scheme = useColorScheme();
 
   return (
-    // <AppearanceProvider>
-    <NavigationContainer
-    // theme={scheme === 'dark' ? DarkTheme : DefaultTheme}
-    >
+   
+    <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Restaurant Categories" component={Categories} options={Header} />
         <Stack.Screen name="Categori Content" component={CategoriContent} />
         <Stack.Screen name="Meel Detail" component={MeelDetail} />
       </Stack.Navigator>
     </NavigationContainer>
-    // </AppearanceProvider>
   );
 }
 
